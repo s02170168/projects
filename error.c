91491ba55a87a36f2add508f616571ea7335e558
s02170168 <s02170168@bitbucket.org/mozgovihws/task5.git>
2019-11-21 00:23:38 +0300

    console colors

--- error.c
+++ error.c
@@ -4,10 +4,10 @@
 void err_msg(err_type err){
     switch (err){
         case syntax:
-            printf("%sSyntax error\n%s", "\033[22;31m", "\033[0m");
+            printf("%sSyntax error%s\n", "\033[22;31m", "\033[0m");
             break;
         case allocation:
-            printf("%sMemory allocation error\n%s", "\033[22;31m", "\033[0m");
+            printf("%sMemory allocation error%s\n", "\033[22;31m", "\033[0m");
             break;
         default:
             break;
